c7af56029a137e81ae0e2519af9679375ad8162f
rousong <recall131477@gmail.com>
2021-08-21 22:34:04 +0800

    finish

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,22 +1,33 @@
 (function ($) {
-  const goSwiper = new Swiper('.card .swiper-container', {
-    loop: true,
-    slidesPerView: 1,
-    spaceBetween: 30,
-    autoplay: {  // 自動撥放
-      delay: 3000,
-      disableOnInteraction: false, // 用手操控swiper後一樣會自動輪播
-    },
-    breakpoints: {
-      768: {
-        slidesPerView: 2,
+  const goSwiper = () => {
+    const cardSwiper = new Swiper('.card .swiper-container', {
+      loop: true,
+      slidesPerView: 1,
+      spaceBetween: 30,
+      autoplay: {  // 自動撥放
+        delay: 3000,
+        disableOnInteraction: false, // 用手操控swiper後一樣會自動輪播
       },
-      992: {
-        slidesPerView: 3,
+      breakpoints: {
+        768: {
+          slidesPerView: 2,
+        },
+        992: {
+          slidesPerView: 3,
+        }
       }
-    }
-  })
+    });
+  }
+
+  const goModal = () => {
+    $('.header__link').on('click', function () {
+      $('.modal').toggleClass('is-active');
+      $('body').toggleClass('overflow-hidden');
+    });
+  };
+
   $(function () {
     goSwiper();
+    goModal();
   });
 })(jQuery);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: true,
+    content: ['./app/**/*.html', './app/**/*.ejs'],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
@@ -27,7 +30,8 @@ module.exports = {
         '2xl': '1.75rem', // 28px
         '3xl': '2rem',    // 32px
         '4xl': '2.25rem', // 36px
-        '5xl': '2.5rem'   // 40px
+        '5xl': '2.5rem',  // 40px
+        '6xl': '2.75rem'  // 44px
       },
       screens: {
         'sm': '576px',
@@ -39,9 +43,19 @@ module.exports = {
         '3.5': '0.875rem',
         '4.5': '1.125rem',
         '7.5': '1.875rem',
+        '13': '3.25rem',
         '15': '3.75rem',
-        '25': '6.25rem'
+        '16.25': '4.0625rem',
+        '25': '6.25rem',
+        '30': '7.5rem',
+        '40': '10rem',
+        '54.5': '13.625rem'
       },
+      height: {
+        '21': '5.25rem',
+        '100': '25rem',
+        '140': '35rem'
+      }
     },
   },
   variants: {
