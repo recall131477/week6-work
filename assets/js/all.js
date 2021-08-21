e27e081771207f14ef8866da7132db3717220dda
rousong <recall131477@gmail.com>
2021-08-21 22:36:53 +0800

    Update 2021-08-21T14:36:47.089Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,38 @@
 "use strict";
 
-console.log('Hello!');
-$(document).ready(function () {
-  console.log('HesSchool Hello!');
-});
+(function ($) {
+  var goSwiper = function goSwiper() {
+    var cardSwiper = new Swiper('.card .swiper-container', {
+      loop: true,
+      slidesPerView: 1,
+      spaceBetween: 30,
+      autoplay: {
+        // 自動撥放
+        delay: 3000,
+        disableOnInteraction: false // 用手操控swiper後一樣會自動輪播
+
+      },
+      breakpoints: {
+        768: {
+          slidesPerView: 2
+        },
+        992: {
+          slidesPerView: 3
+        }
+      }
+    });
+  };
+
+  var goModal = function goModal() {
+    $('.header__link').on('click', function () {
+      $('.modal').toggleClass('is-active');
+      $('body').toggleClass('overflow-hidden');
+    });
+  };
+
+  $(function () {
+    goSwiper();
+    goModal();
+  });
+})(jQuery);
 //# sourceMappingURL=all.js.map
